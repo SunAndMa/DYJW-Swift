39db39e4a77cd2be64fba8a4067995a264509fed
FlyKite <DogeFlyKite@gmail.com>
2017-12-05 19:02:53 +0800

    Create views in LoginPanel

--- DYJW/iPhone/EduSystem/Controller/EduSystemController.swift
+++ DYJW/iPhone/EduSystem/Controller/EduSystemController.swift
@@ -9,8 +9,12 @@
 import UIKit
 
 class EduSystemController: FKBaseController {
+    
+    fileprivate let loginPanel = LoginPanel.loadFromNib()
+    fileprivate var loginPanelTopConstraint: NSLayoutConstraint!
 
     override func viewDidLoad() {
+        self.setupLoginPanel()
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -21,6 +25,39 @@ class EduSystemController: FKBaseController {
         // Dispose of any resources that can be recreated.
     }
     
+    fileprivate func setupLoginPanel() {
+        self.view.addSubview(self.loginPanel)
+        let topConstraint = NSLayoutConstraint(item: self.view,
+                                               attribute: .top,
+                                               relatedBy: .equal,
+                                               toItem: self.loginPanel,
+                                               attribute: .top,
+                                               multiplier: 1,
+                                               constant: -16)
+        let heightConstraint = NSLayoutConstraint(item: self.loginPanel,
+                                               attribute: .height,
+                                               relatedBy: .equal,
+                                               toItem: nil,
+                                               attribute: .height,
+                                               multiplier: 1,
+                                               constant: 284)
+        let leftConstraint = NSLayoutConstraint(item: self.view,
+                                                attribute: .leading,
+                                                relatedBy: .equal,
+                                                toItem: self.loginPanel,
+                                                attribute: .leading,
+                                                multiplier: 1,
+                                                constant: -16)
+        let rightConstraint = NSLayoutConstraint(item: self.view,
+                                                 attribute: .trailing,
+                                                 relatedBy: .equal,
+                                                 toItem: self.loginPanel,
+                                                 attribute: .trailing,
+                                                 multiplier: 1,
+                                                 constant: -16)
+        self.loginPanelTopConstraint = topConstraint
+        NSLayoutConstraint.activate([topConstraint, heightConstraint, leftConstraint, rightConstraint])
+    }
 
     /*
     // MARK: - Navigation
--- DYJW/iPhone/EduSystem/View/LoginPanel.swift
+++ DYJW/iPhone/EduSystem/View/LoginPanel.swift
@@ -10,12 +10,8 @@ import UIKit
 
 class LoginPanel: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    static func loadFromNib() -> LoginPanel {
+        return Bundle.main.loadNibNamed("LoginPanel", owner: nil, options: nil)?.first as! LoginPanel
     }
-    */
 
 }
